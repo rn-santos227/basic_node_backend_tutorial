1e72f958d0027be6b7fc4cacdf592fa88c770535
Aren <rnsantos227@gmail.com>
2020-11-04 10:03:03 +0800

    change url for update and delete

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.post('/tasks/create', (request, response) => {
 })
 
 //Basic POST function that update a value in the database. URL = localhost:3000/tasks/update
-app.post('/task/update', (request, response) => {
+app.post('/tasks/update', (request, response) => {
     const id = request.body.id
     const title = request.body.title
     const activity = request.body.activity
@@ -68,7 +68,7 @@ app.post('/task/update', (request, response) => {
 })
 
 //Basic POST function that deletes a value in the database. URL = localhost:3000/tasks/delete
-app.post('/task/delete', (request, response) => {
+app.post('/tasks/delete', (request, response) => {
     const id = request.body.id
     const sql = `DELETE FROM tasks WHERE id = ${id}`;
     con.query(sql, function (err, result) {
